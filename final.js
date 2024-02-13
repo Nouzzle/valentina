648140456bf432bea54363cfe519492443e309ca
vinn <90256960+Nouzzle@users.noreply.github.com>
2024-02-13 23:11:57 +0700

    Update final.js
    add somethin in the text

--- final.js
+++ final.js
@@ -6,7 +6,7 @@ document.getElementById('reveal').addEventListener('click', function() {
     var image = document.getElementById('image');
 
     if (button.textContent === "Reveal Message") {
-        message.textContent = "Last but not least, i just wanna to say thank uu for your love and everything u’ve done for me. ur presence in my life has filled it with so much joy and happiness. u’ve stood by me in times of hardship, celebrated with me in times of joy, n hv always been my constant source of support and inspiration. I am incredibly lucky to have u in my life. As we continue this journey together, i look forward to creating more beautiful memories with uu xixi. i love uuu so much. hepi valentine, doain emyu menang pls 😿🙏";
+        message.textContent = "Last but not least, i just wanna to say thank uu for your love and everything u’ve done for me. ur presence in my life has filled it with so much joy and happiness. u’ve stood by me in times of hardship, celebrated with me in times of joy, n hv always been my constant source of support and inspiration. iam incredibly lucky to have u in my life. as we continue this journey together, i look forward to creating more beautiful memories with uu xixi. moga u masuk fh ui ya, aamiin. i love uuu so much. hepi valentine, doain emyu menang pls 😿🙏";
         image.src = "https://i.pinimg.com/564x/22/b9/90/22b990829596bf6cd6b4d4bc287e1294.jpg"; // Replace with the URL of your image
         image.style.display = "block"; // This will make the image visible
         button.textContent = "Hide Message";
